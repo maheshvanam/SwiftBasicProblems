38a70b6ec8e03ad0a75fa7623ce563b061d880e0
Mahesh Vanam <maheshvanam09@gmail.com>
2020-05-20 15:35:39 +0530

    [Mahesh]ADDED:added readme file

---
+++ RandomNumbers/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  RandomNumbers
+//
+//  Created by admin on 20/05/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftPracticeProblems/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftPracticeProblems
+//
+//  Created by admin on 20/05/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
